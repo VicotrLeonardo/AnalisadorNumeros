89a222195cc0763c346965b19527bae88c68be52
Victor Leonardo <victorLeoADS@outlook.com>
2022-01-27 08:21:27 -0300

    Segunda Versão do Projeto

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 var botaoAdicionar = document.querySelector(".bloco-botaoAdicionar");
 var lista = [];
-
-function verificar(num, lista) {
-  if (num < 1 && num > 100 && lista.indexOf(num) != -1) {
-    alert("Número Invalido ou já adicionado na lista");
-    return false;
-  } else {
-    return true;
-  }
-}
+var campoTexto = document.querySelector('.bloco-select');
 
 botaoAdicionar.addEventListener("click", function () {
+
+  function verificar(num, lista) {
+    if ((num < 1 || num > 100) || (lista.indexOf(Number(num)) != -1)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   var num = Number(document.querySelector("input#num").value);
+  
   if (verificar(num, lista)) {
     lista.push(num);
     console.log(lista);
+    campoTexto.innerHTML += `<option>O valor ${num} foi adicionado</option>`
+
+  }else{
+    alert("Número Invalido ou já adicionado na lista");
   }
 });
